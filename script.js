f0003227998929ad2710f3bf0cbb80a56b29c1eb
Thanhvinhcode <vinhaz12300@gmail.com>
2025-12-19 11:36:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const PHONE = "0397670298";
 
 let shops = [
   { id:"daiThang", name:"Đại Thắng Ô Tô" },
-  { id:"shop1", name:"Auto Minh" },
-  { id:"shop2", name:"Sài Gòn Car" },
-  { id:"shop3", name:"Thành Vinh Ô TÔ" }
+  { id:"shop1", name:"Thành Vinh Ô TÔ" },
+  { id:"shop2", name:"Chuyên Hợp Tác Bán Xe Ô TÔ Miền Nam" },
+  { id:"shop3", name:"UY TÍN _ LÂU DÀI" }
 ];
 
 let allCars = [];
@@ -26,19 +26,17 @@ function car(name, price, shop, note, img){
 
 function initPage(){
   allCars = [
-    car("Vios 2019",265,"daiThang","Xe gia đình, máy zin","https://i.postimg.cc/fTfq4jYf/elantra2016.png"),
-    car("Innova 2020",420,"daiThang","7 chỗ rộng, xe đẹp"),
+    car("Vios 2019",265,"daiThang","Vios 2018 from mới 2019, số sàn động cơ 1.5, odo 7.7v. Nội ngoại thất đẹp","https://i.postimg.cc/g2wMw9fV/Vios-2018.png"),
+    car("Huyndai Elantra 2016 ",295,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/fTfq4jYf/elantra2016.png"),
+    car("Mitsubishi 2004 ",79,"daiThang","Xe bản đặc biệt 8 chỗ ngoài , nội thất khá mới , hộp số chuyển động nhẹ nhàng ","https://i.postimg.cc/4yd6rVGh/jolie.png"),
+    car("Innova 2019",385,"daiThang","Innova 2019 số sàn bản E. động cơ 2.0.màu trắng, máy còn mới, được trang bị camera, màn hình đầy đủ, ghế da còn mới, nét căng","https://i.postimg.cc/VkBRTj09/innova2019.png"),
+
+    car("Đợi chủ đầu tư hoa hồng ",3,"shop1","hợp tác bán xe","https://i.postimg.cc/JhssJ88j/profile.png"),
+    car("Đợi chủ đầu tư hoa hồng ",3,"shop2","hợp tác bán xe","https://i.postimg.cc/JhssJ88j/profile.png"),
+    car("Đợi chủ đầu tư hoa hồng ",3,"shop3","hợp tác bán xe","https://i.postimg.cc/JhssJ88j/profile.png"),
 
 
-
-    car("Xpander 2019",395,"shop1","Một chủ từ đầu"),
-    car("Kia K3 2018",325,"shop1","Sedan tiết kiệm"),
-
-
-    car("Mazda 3 2020",450,"shop2","Xe cá nhân"),
-    car("i10 2017",180,"shop2","Giá rẻ dễ mua"),
-
-    car("i10 2015",145,"shop3","Giá rẻ dễ mua"),
+   
 
 
 
