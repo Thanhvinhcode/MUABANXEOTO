fcb2a82312ae5dab3fe6d4a372c0ab7262bfbaee
Thanhvinhcode <vinhaz12300@gmail.com>
2025-12-19 11:50:45 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -26,10 +26,14 @@ function car(name, price, shop, note, img){
 
 function initPage(){
   allCars = [
-    car("Vios 2019",265,"daiThang","Vios 2018 from mới 2019, số sàn động cơ 1.5, odo 7.7v. Nội ngoại thất đẹp","https://i.postimg.cc/g2wMw9fV/Vios-2018.png"),
-    car("Huyndai Elantra 2016 ",295,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/fTfq4jYf/elantra2016.png"),
-    car("Mitsubishi 2004 ",79,"daiThang","Xe bản đặc biệt 8 chỗ ngoài , nội thất khá mới , hộp số chuyển động nhẹ nhàng ","https://i.postimg.cc/4yd6rVGh/jolie.png"),
-    car("Innova 2019",385,"daiThang","Innova 2019 số sàn bản E. động cơ 2.0.màu trắng, máy còn mới, được trang bị camera, màn hình đầy đủ, ghế da còn mới, nét căng","https://i.postimg.cc/VkBRTj09/innova2019.png"),
+    car("Vios 2018 SS",265,"daiThang","Vios 2018 from mới 2019, số sàn động cơ 1.5, odo 7.7v. Nội ngoại thất đẹp","https://i.postimg.cc/g2wMw9fV/Vios-2018.png"),
+    car("Huyndai Elantra 2016 SS",295,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/fTfq4jYf/elantra2016.png"),
+    car("Mitsubishi 2004 SS ",79,"daiThang","Xe bản đặc biệt 8 chỗ ngoài , nội thất khá mới , hộp số chuyển động nhẹ nhàng ","https://i.postimg.cc/4yd6rVGh/jolie.png"),
+    car("Kia K3 2015 STĐ ",325,"daiThang","Innova 2019 số sàn bản E. động cơ 2.0.màu trắng, máy còn mới, được trang bị camera, màn hình đầy đủ, ghế da còn mới, nét căng","https://i.postimg.cc/VkBRTj09/innova2019.png"),
+    car("Vios 2016 STĐ ",290,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/SKPD7mz3/vios-2016.png"),
+    car("Huyndai i10 2014 SS",145,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/t45cP05L/i10-2014.png"),
+    car("Huyndai i10 2016 SS Bản Đủ",185,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/jqDrbjX7/i10-2016.png"),
+    car("Huyndai i10 2018 STĐ Bản đủ ",275,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/JzcfqH1S/i10-2018.png"),
 
     car("Đợi chủ đầu tư hoa hồng ",3,"shop1","hợp tác bán xe","https://i.postimg.cc/JhssJ88j/profile.png"),
     car("Đợi chủ đầu tư hoa hồng ",3,"shop2","hợp tác bán xe","https://i.postimg.cc/JhssJ88j/profile.png"),
