ac96f7e7a8d3221c2465416705f18c1459e6122d
Thanhvinhcode <vinhaz12300@gmail.com>
2025-12-20 15:52:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,23 +12,26 @@ let cars = [];
 let currentShop = "daiThang";
 let minPrice = 0, maxPrice = 9999;
 
-function car(name, price, shop, note, img){
+function car(name, price, shop, note, imgs){
   return {
     name,
     price,
     shop,
     note,
     phone: PHONE,
-    img: img || "https://via.placeholder.com/400"
+    imgs: Array.isArray(imgs) ? imgs : [imgs]
   };
 }
 
 
+
 function initPage(){
   allCars = [
-    car("Toyota Vios 2018 SS",265,"daiThang","Vios 2018 from mới 2019, số sàn động cơ 1.5, odo 7.7v. Nội ngoại thất đẹp","https://i.postimg.cc/g2wMw9fV/Vios-2018.png"),
+    car("Toyota Vios 2018 SS",265,"daiThang","Vios 2018 from mới 2019, số sàn động cơ 1.5, odo 7.7v. Nội ngoại thất đẹp",["https://i.postimg.cc/g2wMw9fV/Vios-2018.png","https://i.ibb.co/Q7c2K6k3/image.png","https://i.ibb.co/tPmdLdr8/image.png","https://i.ibb.co/N2MyqKd8/image.png","https://i.ibb.co/PvhP46dx/image.png"]),
     car("Hyundai Elantra 2016 SS",295,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/fTfq4jYf/elantra2016.png"),
-    car("Mitsubishi 2004 SS ",79,"daiThang","Xe bản đặc biệt 8 chỗ ngoài , nội thất khá mới , hộp số chuyển động nhẹ nhàng ","https://i.postimg.cc/4yd6rVGh/jolie.png"),
+
+    car( "Mitsubishi 2004 SS",79,"daiThang","Xe bản đặc biệt 8 chỗ ngoài , nội thất khá mới , hộp số chuyển động nhẹ nhàng ",["https://i.postimg.cc/4yd6rVGh/jolie.png","https://i.postimg.cc/ZnVZFs4f/image.png","https://i.postimg.cc/QdjYrNQR/image.png","https://i.ibb.co/wth1DP6/image.png","https://i.ibb.co/TMgbc4JT/image.png","https://i.ibb.co/Kz9BKRBw/image.png"]),
+
     car("Toyota Innova 2019 SS ",325,"daiThang","Innova 2019 số sàn bản E. động cơ 2.0.màu trắng, máy còn mới, được trang bị camera, màn hình đầy đủ, ghế da còn mới, nét căng","https://i.postimg.cc/VkBRTj09/innova2019.png"),
     car("Toyota Vios 2016 STĐ ",290,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/SKPD7mz3/vios-2016.png"),
     car("Hyundai i10 2014 SS",145,"daiThang","Xe gia đình,không qua dịch vụ","https://i.postimg.cc/t45cP05L/i10-2014.png"),
@@ -107,7 +110,7 @@ function renderCars(){
   cars.forEach((c,i)=>{
     carList.innerHTML += `
       <div class="car-card" onclick="showDetail(${i})">
-        <img src="${c.img}">
+<img src="${c.imgs[0]}">
         <div class="info">
           <h4>${c.name}</h4>
           <div class="price">${c.price} triệu</div>
@@ -119,7 +122,25 @@ function renderCars(){
 function showDetail(i){
   const c = cars[i];
   detailName.innerText = c.name;
-  detailImg.src = c.img;
+detailImg.src = c.imgs[0];
+detailThumbs.innerHTML = "";
+
+c.imgs.forEach((src, index) => {
+  const img = document.createElement("img");
+  img.src = src;
+
+  if(index === 0) img.classList.add("active");
+
+  img.onclick = () => {
+    detailImg.src = src;
+    document
+      .querySelectorAll("#detailThumbs img")
+      .forEach(i => i.classList.remove("active"));
+    img.classList.add("active");
+  };
+
+  detailThumbs.appendChild(img);
+});
   detailPrice.innerText = c.price + " triệu";
   detailNote.innerText = c.note;
 
